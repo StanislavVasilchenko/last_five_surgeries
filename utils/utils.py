0b368d5d5fceda10119cf57461cd1b33cd1cb5b8
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-09 21:08:55 +0300

    added comments to func

--- utils/utils.py
+++ utils/utils.py
@@ -3,27 +3,37 @@ import json
 from constants import PATH
 
 
-def read_file(path=PATH):
-    """Читает данные из файла"""
+def read_file(path=PATH) -> list:
+    """Считывает данные из json файла и возвращает список словарей"""
     with open(path, "r") as file:
         result = json.load(file)
     return result
 
 
-def last_transactions(data: list):
+def last_transactions(data: list, key="state", value="EXECUTED") -> list:
+    """Принимает на вход список словарей и собирает новый список отфильтрованный
+    по определенному ключу (по умолчанию "state") и значению этого ключа (по умодчанию "EXECUTED")
+    Возвращает список из пяти словарей с искомым ключ=значение"""
     five_executed_transactions = []
     for transaction in data:
-        if transaction.get("state") == "EXECUTED" and len(five_executed_transactions) < 5:
+        if transaction.get(key) == value and len(five_executed_transactions) < 5:
             five_executed_transactions.append(transaction)
     return five_executed_transactions
 
 
-def format_date(date: str):
+def format_date(date: str) -> str:
+    """Принимает на вход строку с датой в полном формате (2018-01-26T15:40:13.413061) и возвращает строку
+    в формате день.месяц.год (26.01.2018)"""
     date_operation = date.split('T')[0].split("-")
     return ".".join(date_operation[::-1])
 
 
 def hide_number(number: str):
+    """Принимает на вход строку с платежными данными (Счет 44812258784861134719) и возвращает
+    строку со скрытым номером счета или карты:
+    Счет **4719
+    Visa Classic 6831 98** **** 7658
+    """
     broken_number = number.split(" ")
     for digits in broken_number:
         if digits.isdigit() and len(digits) > 16:
